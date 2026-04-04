ddb45ffcf9d111c86005ae8d3f6b63c143ac3698
Pradeepa S <pradeepa16022006@gmail.com>
2026-04-04 11:19:46 +0530

    version 2 updated

--- README.md
+++
@@ -1,4 +0,0 @@
-🌐 Personal Portfolio Website  
-
-A personal portfolio website built with **HTML, CSS, and JavaScript** to showcase my **skills, projects, and accomplishments** as a web developer.  
-The portfolio is designed with an attractive and visually appealing layout to capture visitors' attention and highlight my professional journey.  
--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 function toggleMode() {
   document.body.classList.toggle('dark');
   const btn = document.querySelector('.mode-toggle');
+
   if (document.body.classList.contains('dark')) {
     btn.textContent = "☀️";
   } else {
     btn.textContent = "🌙";
   }
 }
-=======
-function toggleMode() {
-  document.body.classList.toggle('dark');
-  const btn = document.querySelector('.mode-toggle');
-  if (document.body.classList.contains('dark')) {
-    btn.textContent = "☀️";
-  } else {
-    btn.textContent = "🌙";
-  }
-}
->>>>>>> 46247de4797938bbd2c7a8d10a9334b02a1c3c92
